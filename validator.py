c2152b5349e51cafd190900df009b0309320ebce
= <Neugebauer_David@web.de>
2013-02-24 08:14:46 +0100

    Added basic functionality to validator.

--- validator.py
+++ validator.py
@@ -7,4 +7,13 @@ class Validator:
 		self.value_spaces = {}
 
 	def add_value_space(self, spacename, values):
-		self.value_spaces[spacename] = values;
+		self.value_spaces[spacename] = values
+
+	def get_value_space(self, spacename):
+		return self.value_spaces.get(spacename, None)
+
+	def validate(self, spacename, value):
+		return (value in self.get_value_space(spacename))
+	
+	# def validate(self, )
+
