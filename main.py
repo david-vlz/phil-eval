a0d694844d5662107319dcb9d6843a801917b3a6
= <Neugebauer_David@web.de>
2013-03-03 20:47:17 +0100

    Added preparation functions to PhilRecord.

--- main.py
+++ main.py
@@ -10,8 +10,37 @@ from mappings import mappings
 class PhilRecord(Record):
 
 	def prepare(self):
-		field = self.get_field_by_column_name(u'Zeit Vorbereitung');
-		self.to_ger_float(field);
+		col_names = [u'Zeit Vorbereitung Intensiv', 
+			u'Zeit Nachbereitung Intensiv',
+			u'Zeit Lesen Intensiv',
+			u'Leistungspunkte Intensiv',
+			u'Art Veranstaltung Intensiv',
+			u'Zeit Vorbereitung Durchschnittlich', 
+			u'Zeit Nachbereitung Durchschnittlich',
+			u'Zeit Lesen Durchschnittlich',
+			u'Leistungspunkte Durchschnittlich',
+			u'Art Veranstaltung Durchschnittlich',
+			u'Arbeitszeit']
+		for name in col_names:
+			field = self.get_field_by_column_name(name)
+			self.to_ger_float(field)
+
+		col_names = [u'Art Veranstaltung Intensiv',
+			u'Art Veranstaltung Durchschnittlich']
+		for name in col_names:
+			field = self.get_field_by_column_name(name)
+			self.extract_additional_value(field, 'Andere: ')
+
+		col_names = [u'Berufliches Ziel',
+			u'Gr\xfcnde Fehlen Obligatorische',
+			u'Was hat abgehalten?']
+		for name in col_names:
+			field = self.get_field_by_column_name(name)
+			self.extract_additional_value(field, 'Sonstiges: ')
+
+		field = self.get_field_by_column_name(u'Einkunft Sonstiges:')
+		self.extract_additional_value(field, 'Ja, ')
+
 
 	def to_ger_float(self, field):
 		if type(field.value) != float:
@@ -19,7 +48,17 @@ class PhilRecord(Record):
 			if ',' in no_str:
 				elems = no_str.split(',')
 				if len(elems) == 2:
-					field.value = float(elems[0]) + (float(elems[1])/10)
+					try:
+						field.value = float(elems[0]) + (float(elems[1])/10)
+					except:
+						print 'Error in Conversion: ', field.y, field.column.name
+
+	def extract_additional_value(self, field, value_introduction_string):
+		if field.value.find(value_introduction_string) == 0:
+			value = field.value
+			intro_end_index = len(value_introduction_string)
+			field.value = value[:intro_end_index]
+			field.additional_value = value[intro_end_index:]
 
 
 
