45a8583162556f5f23fa40f2c64f1f5a49e37f00
= <Neugebauer_David@web.de>
2013-03-03 16:58:10 +0100

    Added custom Record class PhilRecord to main script.

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ import xlrd
 
 from stats import *
 from validate import *
+from mappings import mappings
+
+class PhilRecord(Record):
+
+	def prepare(self):
+		field = self.get_field_by_column_name(u'Zeit Vorbereitung');
+		self.to_ger_float(field);
+
+	def to_ger_float(self, field):
+		if type(field.value) != float:
+			no_str = field.value
+			if ',' in no_str:
+				elems = no_str.split(',')
+				if len(elems) == 2:
+					field.value = float(elems[0]) + (float(elems[1])/10)
+
+
 
 # Excel-Tabellen bereithalten
 book = xlrd.open_workbook('data.xls')
@@ -21,4 +38,17 @@ for i in range(0, len(valid_fields_sheet.row(0))):
 		field_list.append(column[j].value)
 	validator.add_value_space(name, [x for x in field_list if x != ''])
 
+# Aliase an Validator übergeben
+for k, v in mappings.items():
+	validator.add_spacename_alias(k, v)
 
+t = Table(main_sheet, PhilRecord, validator)
+r = t.records[11]
+
+
+def check(column_nr):
+	t = Table(main_sheet, PhilRecord, validator)
+	t.prepare_all();
+	invs = t.get_invalids_by_column(column_nr)
+	for field in invs:
+		print (field.y, repr(field.value), field.column.name)
